a44ea75d10a78cbbc476d34b3fb8246f8cb69bdd
g-adil-r <112382916+g-adil-r@users.noreply.github.com>
2023-11-12 21:44:49 +0700

    Update Production.php
    Update model agar menggunakan tabel dataproduction

--- lumenapi/app/Models/Production.php
+++ lumenapi/app/Models/Production.php
@@ -3,18 +3,25 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 class Production extends Model
 {
-/**
-* The attributes that are mass assignable.
-*
-* @var array
-*/
-protected $fillable = [
-'nama_item', 'jumlah_item', 'harga_item'
-];
-/**
-* The attributes excluded from the model's JSON form.
-*
-* @var array
-*/
-protected $hidden = [];
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'dataproduction';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nama_item', 'jumlah_item', 'harga_item'
+    ];
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
 }
