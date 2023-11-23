3e97952f2f4357ffb83910b26b4aa1697d0c5eb3
auliasanggraini <112447425+auliasanggraini@users.noreply.github.com>
2023-11-23 16:14:03 +0700

    finance_report

--- lumenapi/database/migrations/2023_11_23_090350_create_finance_report_table.php
+++ lumenapi/database/migrations/2023_11_23_090350_create_finance_report_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+
         Schema::create('finance_report', function (Blueprint $table) {
             $table->id();
             $table->string('nama_transaksi');
